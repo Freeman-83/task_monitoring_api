21d15965005cfc23b9fc3059d473fcca9a462c03
Михаил Марин <mmn-83@yandex.ru>
2025-05-09 10:43:22 +0300

    slc

--- task_monitoring/task_monitoring/settings.py
+++ task_monitoring/task_monitoring/settings.py
@@ -184,7 +184,8 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 # ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+UPLOADED_ROOT = BASE_DIR / 'uploaded'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
