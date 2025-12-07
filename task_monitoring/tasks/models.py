a15e7d31af6ff899b04a7763634d49285826f714
Михаил Марин <mmn-83@yandex.ru>
2025-12-07 23:52:44 +0300

    working on correspondence models

--- task_monitoring/tasks/models.py
+++ task_monitoring/tasks/models.py
@@ -4,6 +4,107 @@ from django.db import models
 from departments.models import Employee
 
 
+class Document(models.Model):
+    """Модель Документа."""
+
+    document_application = models.FileField(
+        verbose_name='',
+        upload_to='docs/',
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=['doc', 'docx', 'pdf', 'jpg', 'jpeg', 'png']
+            )
+        ],
+        help_text='Разрешенные расширения: doc, docx, pdf, jpg, jpeg, png.',
+        blank=True,
+        null=True
+    )
+
+
+class Correspondence(Document):
+    """Модель корреспонденции."""
+
+    correspondence_type = models.CharField(
+        verbose_name='Тип корреспонденции',
+        max_length=56
+    )
+    registration_number = models.CharField(
+        verbose_name='Номер регистрации',
+        max_length=56
+    )
+    registration_date = models.DateField(
+        verbose_name='Дата регистрации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+
+class IncomingCorrespondence(Correspondence):
+    """Входящая корреспонденция."""
+
+    incoming_number = models.CharField(
+        verbose_name='Входящий номер',
+        max_length=56,
+        blank=True,
+        null=True
+    )
+    incoming_date = models.DateField(
+        verbose_name='Входящая дата',
+        auto_now_add=True,
+        db_index=True
+    )
+    incoming_sender = models.CharField(
+        verbose_name='Отправитель',
+        max_length=56,
+        blank=True,
+        null=True
+    )
+    incoming_sender_person = models.CharField(
+        verbose_name='Подписант',
+        max_length=56,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.incoming_sender
+
+
+class OutgoingCorrespondence(Correspondence):
+    """Исходящая корреспонденция."""
+    
+    outgoing_sender_person = models.CharField(
+        verbose_name='Подписант',
+        max_length=56,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.outgoing_sender_person
+    
+
+class RegulatoryLegalAct(Document):
+    """Модель номативно-правового акта."""
+
+    title = outgoing_sender_person = models.CharField(
+        verbose_name='Подписант',
+        max_length=1000
+    )
+    rla_number = models.CharField(
+        verbose_name='Номер',
+        max_length=56
+    )
+    rla_date = models.DateField(
+        verbose_name='Входящая дата',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
 class Group(models.Model):
     """Модель типа поручения."""
 
@@ -75,6 +176,22 @@ class Task(models.Model):
         verbose_name='Дата исполнения',
         db_index=True
     )
+    task_application = models.ForeignKey(
+        Document,
+        verbose_name='Документация для исполнения',
+        related_name='task_applications'
+    )
+    execution_application = models.ForeignKey(
+        Document,
+        verbose_name='Приложение к исполнению',
+        related_name='execution_applications'
+    )
+    executions_comment = models.TextField(
+        verbose_name='Комментарий к исполнению',
+        max_length=10000,
+        blank=True,
+        null=True
+    )
     is_closed = models.BooleanField(
         verbose_name='Отметка об исполнении поручения инициатором',
         default=False,
@@ -85,36 +202,7 @@ class Task(models.Model):
         default=False,
         db_index=True
     )
-    tasks_application = models.FileField(
-        verbose_name='Приложение к поручению',
-        upload_to='uploaded/tasks_files/',
-        validators=[
-            FileExtensionValidator(
-                allowed_extensions=['doc', 'docx', 'pdf', 'jpg', 'jpeg', 'png']
-            )
-        ],
-        help_text='Разрешенные расширения: doc, docx, pdf, jpg, jpeg, png.',
-        blank=True,
-        null=True
-    )
-    executions_application = models.FileField(
-        verbose_name='Приложение к исполнению',
-        upload_to='uploaded/applications/',
-        validators=[
-            FileExtensionValidator(
-                allowed_extensions=['doc', 'docx', 'pdf', 'jpg', 'jpeg', 'png']
-            )
-        ],
-        help_text='Разрешенные расширения: doc, docx, pdf, jpg, jpeg, png.',
-        blank=True,
-        null=True
-    )
-    executions_comment = models.TextField(
-        verbose_name='Комментарий к исполнению',
-        max_length=10000,
-        blank=True,
-        null=True
-    )
+    
 
     class Meta:
         ordering = ['execution_date']
